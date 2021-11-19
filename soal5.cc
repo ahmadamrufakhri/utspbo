20723235e8ee3acd8b19cc7f79e491afa916c7e7
amru fakhri <81955084+ahmadamrufakhri@users.noreply.github.com>
2021-11-19 18:57:45 +0700

    jawaban soal nomor 5

---
+++ soal5.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   printf("Masukan Angka Pertama : %d\n", 10);
+   printf("Masukan Angka Kedua : %d\n", 2);
+   printf("Penjumlahan : %d\n", 12);
+   printf("Pengurangan : %d\n", 8);
+   printf("Perkalian : %d\n", 20);
+   printf("Pembagian : %d\n", 5);
+
+    return 0;
+}
